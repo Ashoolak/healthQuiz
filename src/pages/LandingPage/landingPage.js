23543fe7832ce9015e7434fffbd1328b9b5b78d4
Ashkan Yazdi Zadeh <55773710+Ashoolak@users.noreply.github.com>
2023-10-19 20:47:12 -0400

    Removed ts-particles

--- src/pages/LandingPage/landingPage.js
+++ src/pages/LandingPage/landingPage.js
@@ -1,37 +1,8 @@
-import React, { useEffect, useState } from 'react';
-import Particles from 'react-particles-js';
+import React from 'react';
 
 export default function MergedLandingPage(props) {
-  const [particleNumber, setParticleNumber] = useState(30);
-
-  useEffect(() => {
-    setParticleNumber(window.innerWidth < 768 ? 20 : 50);
-    // Update particle number if window is resized
-    const handleResize = () => {
-      setParticleNumber(window.innerWidth < 768 ? 20 : 50);
-    };
-    window.addEventListener('resize', handleResize);
-    return () => {
-      window.removeEventListener('resize', handleResize);
-    };
-  }, []);
-
   return (
     <div className="container mx-auto h-screen flex flex-col justify-center items-center relative">
-      <Particles
-        className="absolute inset-0"
-        params={{
-          particles: {
-            number: { value: particleNumber },
-            size: { random: true, value: 40 },
-            shape: { type: 'circle' },
-            opacity: { value: 0.7, random: true },
-            move: { speed: 1, out_mode: 'bounce' },
-            color: { value: ['#0d47a1', '#6200ea'] },
-            collisions: { enable: false },
-          },
-        }}
-      />
       <div className="relative z-3 text-center">
         <img
           src="/images/prepare.png"
