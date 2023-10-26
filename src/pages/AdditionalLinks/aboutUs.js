151cc7ac12d852aef9ed433410110e1b375d43e7
Ashkan Yazdi Zadeh <55773710+Ashoolak@users.noreply.github.com>
2023-10-26 19:05:55 -0400

    updated about us page

--- src/pages/AdditionalLinks/aboutUs.js
+++ src/pages/AdditionalLinks/aboutUs.js
@@ -1,11 +1,25 @@
 import React from 'react';
+import Link from 'next/link';
 
 export default function AboutUs() {
   return (
-    <div className="bg-gray-100 min-h-screen flex flex-col justify-center items-center p-10">
-      <h1 className="text-4xl font-extrabold text-blue-700 mb-8">About Us</h1>
+    <div className="bg-gray-900 text-white min-h-screen p-8 flex flex-col justify-center">
+      <div className="mb-10 md:mb-16">
+        <div className="flex items-center justify-center mt-4 md:absolute md:mt-0 md:top-8 md:left-8">
+          <Link
+            href="/CompletionPage/completionPage"
+            className="flex items-center bg-gray-700 p-2 rounded-lg transition duration-300 hover:bg-gray-800 hover:text-white"
+          >
+            <span className="material-icons-outlined mr-2">arrow_back</span>
+            <p className="mr-3">Back to Completion Page</p>
+          </Link>
+        </div>
+        <h1 className="mt-4 text-center text-4xl font-bold tracking-widest text-orange-400 border-b-4 border-orange-300 md:mt-0 md:absolute md:top-8 md:left-1/4 md:w-1/2">
+          About Us
+        </h1>
+      </div>
 
-      <div className="bg-white max-w-2xl shadow-lg rounded-lg p-6 mb-6">
+      <div className="bg-white text-black max-w-2xl mx-auto rounded-lg p-6 mb-6">
         <div className="mb-4">
           <h2 className="text-3xl font-semibold text-gray-800 mb-3">
             Our Team
